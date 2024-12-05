102fd246de202c7e7fb5b304ad8fff296dcc9827
kimleangKL <lokkoemleang@gmail.com>
2024-12-05 12:58:59 +0700

    hfghf

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+
+import 'package:design_up/view/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.grey[800],
+          titleTextStyle: TextStyle(
+            color: Colors.grey[200],
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+          ),
+          centerTitle: true,
+          elevation: 10,
+        ),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
